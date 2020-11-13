1b17d74fc417e7e6db01ae4a58b942e2f60a510f
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2020-11-13 03:05:24 -0500

    Added camera test code

--- src/guimain.cpp
+++ src/guimain.cpp
@@ -33,9 +33,12 @@ extern "C"
 #error "NT kernel not supported"
 #endif
 
+volatile sig_atomic_t done = 0;
+
 static void glfw_error_callback(int error, const char *description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+    done = 1;
 }
 
 const ImVec4 IMRED = ImVec4(1, 0, 0, 1); // RGBA
@@ -184,10 +187,145 @@ bool LoadTextureFromFile(const char *filename, GLuint *out_texture, int *out_wid
     return true;
 }
 
+bool LoadTextureFromMem(const unsigned char *in_jpeg, ssize_t len, GLuint *out_texture, int *out_width, int *out_height)
+{
+    // Load from file
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char *image_data;
+
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    /* More stuff */
+    FILE *infile;      /* source file */
+    JSAMPARRAY buffer; /* Output row buffer */
+    int row_stride;    /* physical row width in output buffer */
+
+    /* In this example we want to open the input file before doing anything else,
+   * so that the setjmp() error recovery below can assume the file is open.
+   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
+   * requires it in order to read binary files.
+   */
+
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    cinfo.err = jpeg_std_error(&jerr);
+    /* Step 1: allocate and initialize JPEG decompression object */
+    jpeg_create_decompress(&cinfo);
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+
+    /* Step 2: specify data source (eg, a file) */
+
+    jpeg_mem_src(&cinfo, in_jpeg, len);
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    /* Step 3: read file parameters with jpeg_read_header() */
+
+    fprintf(stderr, "%s: %d %d\n", __func__, __LINE__, jpeg_read_header(&cinfo, TRUE));
+    /* We can ignore the return value from jpeg_read_header since
+   *   (a) suspension is not possible with the stdio data source, and
+   *   (b) we passed TRUE to reject a tables-only JPEG file as an error.
+   * See libjpeg.txt for more info.
+   */
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    /* Step 4: set parameters for decompression */
+
+    /* In this example, we don't need to change any of the defaults set by
+   * jpeg_read_header(), so we do nothing here.
+   */
+
+    /* Step 5: Start decompressor */
+    cinfo.out_color_space = JCS_EXT_RGBX;
+    cinfo.scale_num = 480; // scale to 480p
+    cinfo.scale_denom = cinfo.image_height;
+    (void)jpeg_start_decompress(&cinfo);
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    /* We may need to do some setup of our own at this point before reading
+   * the data.  After jpeg_start_decompress() we have the correct scaled
+   * output image dimensions available, as well as the output colormap
+   * if we asked for color quantization.
+   * In this example, we need to make an output work buffer of the right size.
+   */
+    /* JSAMPLEs per row in output buffer */
+    row_stride = cinfo.output_width * cinfo.output_components;
+    /* Make a one-row-high sample array that will go away when done with image */
+    buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr)&cinfo, JPOOL_IMAGE, row_stride, 1);
+
+    /* Step 6: while (scan lines remain to be read) */
+    /*           jpeg_read_scanlines(...); */
+
+    /* Here we use the library's state variable cinfo.output_scanline as the
+   * loop counter, so that we don't have to keep track ourselves.
+   */
+    image_data = (unsigned char *)malloc(row_stride * cinfo.output_height);
+    image_height = cinfo.output_height;
+    image_width = cinfo.output_width;
+    int loc = 0;
+    fprintf(stderr, "%s: %d: Width = %d, Height = %d Size = %d\n", __func__, __LINE__, cinfo.output_width, cinfo.output_height, row_stride * cinfo.output_height);
+    while (cinfo.output_scanline < cinfo.output_height)
+    {
+        /* jpeg_read_scanlines expects an array of pointers to scanlines.
+     * Here the array is only one element long, but you could ask for
+     * more than one scanline at a time if that's more convenient.
+     */
+        (void)jpeg_read_scanlines(&cinfo, buffer, 1);
+        /* Assume put_scanline_someplace wants a pointer and sample count. */
+        memcpy(&(image_data[loc]), buffer[0], row_stride);
+        loc += row_stride;
+    }
+
+    /* Step 7: Finish decompression */
+
+    (void)jpeg_finish_decompress(&cinfo);
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    /* We can ignore the return value since suspension is not possible
+   * with the stdio data source.
+   */
+
+    /* Step 8: Release JPEG decompression object */
+
+    /* This is an important step since it will release a good deal of memory. */
+    jpeg_destroy_decompress(&cinfo);
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    /* After finish_decompress, we can close the input file.
+   * Here we postpone it until after no more JPEG errors are possible,
+   * so as to simplify the setjmp error logic above.  (Actually, I don't
+   * think that jpeg_destroy can do an error exit, but why assume anything...)
+   */
+    fclose(infile);
+    if (image_data == NULL)
+        return false;
+
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Upload pixels into texture
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    fprintf(stderr, "%s: %d %d\n", __func__, __LINE__, image_texture);
+    free(image_data);
+    *out_texture = image_texture;
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    *out_width = image_width;
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    *out_height = image_height;
+    fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    return true;
+}
+
 GLuint my_image_texture;
 int my_image_width, my_image_height;
 
+GLuint mmy_image_texture;
+int mmy_image_width, mmy_image_height;
+
 bool ImageWindowStat = false;
+bool CamWindowStat = false;
+bool enable_camera = false;
 
 void MainWindow()
 {
@@ -201,6 +339,18 @@ void MainWindow()
 void ImageWindow(bool *active)
 {
     ImGui::Begin("Image Display", active);
+    if (mmy_image_texture != NULL)
+    {
+        ImGui::Text("pointer = %p", mmy_image_texture);
+        ImGui::Text("size = %d x %d", mmy_image_width, mmy_image_height);
+        ImGui::Image((void *)(intptr_t)mmy_image_texture, ImVec2(mmy_image_width, mmy_image_height));
+    }
+    ImGui::End();
+}
+
+void CamWindow(bool *active)
+{
+    ImGui::Begin("Camera Display", active);
     if (my_image_texture != NULL)
     {
         ImGui::Text("pointer = %p", my_image_texture);
@@ -210,13 +360,49 @@ void ImageWindow(bool *active)
     ImGui::End();
 }
 
+void *update_image(void *ptr)
+{
+    while (!done)
+    {
+        if (enable_camera)
+        {
+            ssize_t len = 0;
+            ucam_snap_picture((ucam *)ptr, &len);
+            unsigned char *img_data = (unsigned char *)malloc(len);
+            ucam_get_data((ucam *)ptr, img_data, len, 0);
+            LoadTextureFromMem(img_data, len, &my_image_texture, &my_image_width, &my_image_height);
+            free(img_data);
+        }
+        usleep(16000); // 16 msec, try to get 60 Hz pictures
+    }
+    return NULL;
+}
+
+#include <termios.h>
 int main(int argc, char *argv[])
 {
+    ucam dev;
+    if (ucam_init(&dev, "/dev/ttyS0", B115200, 11) < 0)
+    {
+        printf("Failed to init, exiting\n");
+        return -1;
+    }
+    dev.pic_mode = 0x0; // compressed jpeg
+    dev.img_fmt = 0x7; // jpeg
+    dev.jpg_res = UCAM_JPG_480p;
+    dev.pkg_sz = 512;
+    if (ucam_sync(&dev) < 0)
+    {
+        printf("Failed to sync, exiting\n");
+        return -1;
+    }
+    ucam_config(&dev, UCAM_INIT);
+    ucam_config(&dev, UCAM_SET_PACK_SZ);
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         return 1;
-    GLFWwindow *window = glfwCreateWindow(1024, 720, "XBand Test System", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1024, 720, "Camera Monitor", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
@@ -242,8 +428,10 @@ int main(int argc, char *argv[])
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    LoadTextureFromFile("test.jpeg", &my_image_texture, &my_image_width, &my_image_height);
+    LoadTextureFromFile("test.jpeg", &mmy_image_texture, &mmy_image_width, &mmy_image_height);
     fprintf(stderr, "%s: %d\n", __func__, __LINE__);
+    pthread_t thr;
+    pthread_create(&thr, NULL, &update_image, (void *)&dev);
     while (!glfwWindowShouldClose(window))
     {
         // Poll and handle events (inputs, window resize, etc.)
@@ -262,6 +450,8 @@ int main(int argc, char *argv[])
         MainWindow();
         if (ImageWindowStat)
             ImageWindow(&ImageWindowStat);
+        if (CamWindowStat)
+            CamWindow(&CamWindowStat);
         // Rendering
         ImGui::Render();
         int display_w, display_h;
@@ -281,6 +471,9 @@ int main(int argc, char *argv[])
         glfwMakeContextCurrent(window);
         glfwSwapBuffers(window);
     }
+    pthread_join(thr, NULL);
+    ucam_hard_rst(&dev);
+    ucam_destroy(&dev);
     // Cleanup
     ImGui_ImplOpenGL2_Shutdown();
     ImGui_ImplGlfw_Shutdown();
